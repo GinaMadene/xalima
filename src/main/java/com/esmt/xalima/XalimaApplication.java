6639031da932bb803d160a58955e6b23d254ee14
GinaMadene <ginamadene@gmail.com>
2024-07-08 12:58:39 +0000

    unit

---
+++ src/main/java/com/esmt/xalima/XalimaApplication.java
@@ -0,0 +1,13 @@
+package com.esmt.xalima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XalimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XalimaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esmt/xalima/XalimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esmt.xalima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XalimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
